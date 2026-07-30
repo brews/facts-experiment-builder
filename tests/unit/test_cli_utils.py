1e65e768464190b8b2f66e79f61abbc8d29e6184
Emma Marshall <55526386+e-marshall@users.noreply.github.com>
2026-07-30 15:32:52 -0600

    rename infra to io + fix root arg (#101)
    * rename infra to io + fix root arg
    
    * remove use of determine_root
    
    * update tests
    
    * remove unused fn
    
    * make arg req
    
    * format
    
    * update readme
    
    ---------
    
    Co-authored-by: Emma Marshall <e.marshall.111@proton.me>

--- tests/unit/test_cli_utils.py
+++ tests/unit/test_cli_utils.py
@@ -3,25 +3,8 @@
 import logging
 
 from facts_experiment_builder.cli.utils import (
-    determine_root,
     configure_logging,
 )
-
-
-def test_determine_root_resolves_correctly_with_alternate_root(tmp_path):
-    cli_root = tmp_path / "my_other_working_dir"
-    cli_root.mkdir()
-    resolved_root = determine_root(cli_root=cli_root)
-    assert resolved_root == cli_root.resolve()
-    assert resolved_root.is_absolute()
-
-
-def test_determine_root_resolves_correctly_with_none(tmp_path, monkeypatch):
-    monkeypatch.chdir(tmp_path)
-    cli_root = None
-    resolved_root = determine_root(cli_root)
-    assert resolved_root == tmp_path.resolve()
-    assert resolved_root.is_absolute()
 
 
 def test_configure_logging_returns_none_correctly():
